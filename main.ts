575d6f9663d7a0938697b32ff4722f5adc740da4
mzwing <mzwing@mzwing.eu.org>
2023-01-15 12:43:26 +0800

    fix: import_map

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-import { serve } from "http/server.ts";
-import staticFiles from "static_files";
+import { serve } from "https://deno.land/std@0.172.0/http/server.ts";
+import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
 const serveFiles = (req: Request) => staticFiles("./")({ 
   request: req, 
   respondWith: (r: Response) => r 
 })
 
-serve((req) => serveFiles(req), { addr: ':8000' });
+serve((req) => serveFiles(req));
