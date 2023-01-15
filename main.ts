49fefad4a496af40532024ad3cf256f73195582f
mzwing <mzwing@mzwing.eu.org>
2023-01-15 15:48:34 +0800

    feat: change the default listening port

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
-import { serve } from "https://deno.land/std@0.172.0/http/server.ts";
+import { serveListener } from "https://deno.land/std@0.172.0/http/server.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const serveFiles = (req: Request) => staticFiles("./")({ 
-  request: req, 
-  respondWith: (r: Response) => r 
-})
+const serveFiles = (req: Request) =>
+  staticFiles("./")({
+    request: req,
+    respondWith: (r: Response) => r,
+  });
 
-serve((req) => serveFiles(req));
+const listener = Deno.listen({ port: 443 });
+
+console.log("Server is running at port 443.");
+
+await serveListener(listener, (req) => serveFiles(req));
