9cb80b2b9c468075b1dbb0be9af0db9465c7e4e3
mzwing <mzwing@mzwing.eu.org>
2024-12-31 23:55:37 +0800

    feat: update to greet the next year!

--- deps.ts
+++
@@ -1,3 +0,0 @@
-export { serve } from "https://deno.land/std@0.172.0/http/server.ts";
-
-export { serveDir } from "https://deno.land/std@0.173.0/http/file_server.ts";
--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
-import { serve, serveDir } from "./deps.ts";
+import { route, type Route } from "@std/http/unstable-route";
+import { serveDir } from "@std/http/file-server";
 
-await serve((req) => {
-  return serveDir(req, {
-    fsRoot: "./",
-  })
-}, {
-  onListen: ({ hostname, port }) => {
-    console.log(`Server is running at ${hostname}:${port}`);
-  },
-});
+const routes: Route[] = [
+  {
+    pattern: new URLPattern({ pathname: "/*" }),
+    handler: (req: Request) => serveDir(req)
+  }
+];
+
+function defaultHandler(_req: Request) {
+  return new Response("Not found", { status: 404 });
+}
+
+Deno.serve(route(routes, defaultHandler));
